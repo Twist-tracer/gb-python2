5439aff7f754c5b88df9963f40ac9431454ce7c3
twist-tracer <twist.tracer@gmail.com>
2021-01-14 23:40:08 +0300

    <lesson6> improved task1

--- lesson6/taks1.py
+++ lesson6/taks1.py
@@ -1,5 +1,6 @@
 from time import sleep
 from itertools import cycle
+from threading import Thread
 
 
 class TrafficLight:
@@ -11,8 +12,6 @@ class TrafficLight:
         self.__color = next(colors)
 
         while True:
-            print(self.__color)
-
             if self.__color == 'красный':
                 sleep(7)
             elif self.__color == 'желтый':
@@ -22,6 +21,24 @@ class TrafficLight:
 
             self.__color = next(colors)
 
+    def get_color(self):
+        return self.__color
+
 
 traffic_light = TrafficLight()
-traffic_light.running()
+thread = Thread(target=traffic_light.running)
+colors = cycle(['красный', 'желтый', 'зеленый'])
+prev_color = current_color = assert_color = None
+thread.start()
+while current_color == assert_color:
+    print(current_color)
+
+    prev_color = current_color
+    current_color = traffic_light.get_color()
+
+    if prev_color != current_color:
+        assert_color = next(colors)
+
+    sleep(1)
+else:
+    print(f'Светофор сломан! Загорелся {current_color} вместо {assert_color}')
